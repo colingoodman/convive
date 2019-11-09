a827fae9e54eca602746eae85c80c68d2c82ee30
Colin G <cgoodman3544@gmail.com>
2019-11-09 16:49:45 -0600

    update senti slither

--- goodman/senti.py
+++ goodman/senti.py
@@ -8,6 +8,11 @@ from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
 
+#database stuff
+client = pymongo.MongoClient('mongodb+srv://johnchapple:hack19password@cluster0-dwemy.mongodb.net/test?retryWrites=true&w=majority')
+db = client.politalkdb
+posts = db.Chats
+
 def print_result(annotations):
 	score = annotations.document_sentiment.score
 	magnitude = annotations.document_sentiment.magnitude
@@ -18,7 +23,9 @@ def print_result(annotations):
 		
 	print('Overall Sentiment: score of {} with magnitude of {}'.format(score, magnitude))
 	
-	return 0
+	pair = [score, magnitude]
+	
+	return pair
 	
 def analyze(filename):
 	client = language.LanguageServiceClient()
@@ -31,7 +38,20 @@ def analyze(filename):
 		type = enums.Document.Type.PLAIN_TEXT)
 	annotations = client.analyze_sentiment(document=document)
 	
-	print_result(annotations)
+	return print_result(annotations)
+	
+def mongo(target, score):
+	#target is the chat ID
+	coin_amt = calc_return(score)
+	
+	posts.update_one({'_id':target}, {"$coin_amt": coin_amt}, upsert=False)
+
+	return 0
+	
+def calc_return(score):
+	coin_count = (1000 * score[0]) + (100 * score[1])
+
+	return coin_count
 	
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(
@@ -42,6 +62,6 @@ if __name__ == '__main__':
 		help = 'The filename of the item you\'d like to analyze.')
 	args = parser.parse_args()
 	
-	analyze(args.filename)
+	set = analyze(args.filename)
 	
 	
