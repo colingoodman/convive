d46ce2c1641606ccaa10c2d4869bf9a7836e06e7
cjwakefield <colin.j.wakefield@gmail.com>
2019-11-09 19:18:15 -0600

    my dog farts and it hurts him

---
+++ goodman/python.js
@@ -0,0 +1,19 @@
+module.exports = 
+{
+
+accountGen : function ()
+{
+    const spawn = require("child_process").spawn;
+    const pythonProcess = spawn('python3',["/home/cwakefield/github/hack19/goodman/account_gen.py" ]); 
+}, 
+
+backup : function(PATH , ObjId)
+{
+    const spawn = require("child_process").spawn;
+    const pythonProcess = spawn('python',["/home/cwakefield/github/hack19/goodman/account_gen.py" , PATH  , ObjId]); 
+}
+
+}
+
+var python = require('./python');
+python.accountGen(); 
